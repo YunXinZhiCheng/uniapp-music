516ce94a4129f129afdeac180a0a6e63622378d9
YunXinZhiCheng <867395636@qq.com>
2021-10-17 21:53:07 +0800

    头部组件结构与布局

---
+++ components/musichead/musichead.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="music-head">
+		<!-- 头部图标 -->
+		<view class="music-head-icon">
+			<text class="iconfont iconzuojiantou-copy"></text> | <text class="iconfont iconshouye"></text>
+		</view>
+		{{title}}
+	</view>
+</template>
+
+<script>
+	export default {
+		// 头部组件
+		name: "musichead",
+		data() {
+			return {
+
+			};
+		},
+		// 接收属性
+		props: ['title']
+	}
+</script>
+
+<style scoped>
+	/* 1px=2rpx */
+	.music-head {
+		width: 100%;
+		height: 75px;
+		font-size: 16px;
+		line-height: 80px;
+		text-align: center;
+		color: #000000;
+		position: relative;
+	}
+
+	/* 头部图标 */
+	.music-head-icon {
+		position: absolute;
+		left: 8px;
+		top: 23px;
+		width: 90px;
+		height: 31px;
+		background: rgba(0, 0, 0, 0.4);
+		color: #FFFFFF;
+		line-height: 31px;
+		border-radius: 15px;
+		display: flex;
+		justify-content: space-evenly;
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,24 @@
 <template>
 	<view class="content">
-	 <text class="iconfont iconlike"></text>
+		<!-- 头部组件 -->
+		<musichead title="云音乐"></musichead>
 	</view>
 </template>
 
 <script>
 	// 字体图标引入
 	import '@/common/iconfont.css'
+	// 头部组件引入
+	import musichead from '../../components/musichead/musichead.vue'
 	export default {
 		data() {
 			return {
 				title: 'Hello'
 			}
+		},
+		// 注册
+		components: {
+			musichead
 		},
 		onLoad() {
 
@@ -22,30 +29,6 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style scoped>
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
