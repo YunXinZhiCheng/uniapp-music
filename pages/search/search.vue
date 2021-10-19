6a79e2c8db9e95133fffc978ef17043ff35620a4
YunXinZhiCheng <867395636@qq.com>
2021-10-19 22:58:02 +0800

    搜索页-热搜榜布局及样式

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -35,6 +35,52 @@
 						<view>测试歌曲</view>
 					</view>
 				</view>
+
+				<!-- 热搜榜 -->
+				<view class="search-hot">
+					<!-- 头部 -->
+					<view class="search-hot-head">热搜榜</view>
+					<!-- 列表项：左中右 -->
+					<view class="search-hot-item">
+						<view class="search-hot-top">1</view>
+						<view class="search-hot-word">
+							<view>
+								忘情水
+								<image src="../../static/dujia.png" mode="aspectFill"></image>
+							</view>
+							<view>"忘情水"这个词实在是太美了</view>
+						</view>
+						<text class="search-hot-count">2968644</text>
+					</view>
+
+					<!-- 列表项：左中右 -->
+					<view class="search-hot-item">
+						<view class="search-hot-top">1</view>
+						<view class="search-hot-word">
+							<view>
+								忘情水
+								<image src="../../static/dujia.png" mode="aspectFill"></image>
+							</view>
+							<view>"忘情水"这个词实在是太美了</view>
+						</view>
+						<text class="search-hot-count">2968644</text>
+					</view>
+
+					<!-- 列表项：左中右 -->
+					<view class="search-hot-item">
+						<view class="search-hot-top">1</view>
+						<view class="search-hot-word">
+							<view>
+								忘情水
+								<image src="../../static/dujia.png" mode="aspectFill"></image>
+							</view>
+							<view>"忘情水"这个词实在是太美了</view>
+						</view>
+						<text class="search-hot-count">2968644</text>
+					</view>
+
+				</view>
+
 			</scroll-view>
 		</view>
 	</view>
@@ -67,7 +113,7 @@
 		display: flex;
 		align-items: center;
 		height: 70rpx;
-		margin: 70rpx 30rpx 30rpx 30rpx;
+		margin: 70rpx 30rpx 50rpx 30rpx;
 		background: #F7F7F7;
 		border-radius: 50rpx;
 	}
@@ -87,7 +133,7 @@
 
 	/* 历史记录 */
 	.search-history {
-		margin: 0 30rpx;
+		margin: 0rpx 30rpx 50rpx 30rpx;
 		font-size: 26rpx;
 	}
 
@@ -110,5 +156,55 @@
 		margin-right: 30rpx;
 		margin-bottom: 30rpx;
 		background: #f7f7f7;
+	}
+
+	/* 热搜榜 */
+	.search-hot {
+		margin: 0 30rpx;
+		font-size: 26rpx;
+	}
+
+	/* 热搜榜 头部 */
+	.search-hot-head {
+		margin-bottom: 36rpx;
+	}
+
+	/* 热搜榜 列表项 */
+	.search-hot-item {
+		display: flex;
+		align-items: center;
+		margin-bottom: 58rpx;
+	}
+
+	/* 左 */
+	.search-hot-top {
+		color: #fb2222;
+		width: 60rpx;
+		margin-left: 8rpx;
+	}
+
+	/* 中 */
+	.search-hot-word {
+		flex: 1;
+	}
+
+	.search-hot-word view:nth-child(1) {
+		font-size: 30rpx;
+		color: #000000;
+	}
+
+	.search-hot-word view:nth-child(2) {
+		font-size: 24rpx;
+		color: #878787;
+	}
+
+	.search-hot-word image {
+		width: 48rpx;
+		height: 22rpx;
+	}
+
+	/* 右 */
+	.search-hot-count {
+		color: #878787;
 	}
 </style>
